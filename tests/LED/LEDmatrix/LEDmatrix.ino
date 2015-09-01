9bff3a7d940886dd622690acf7dae3640c203205
Philipp Schardt <philipp@stud.uni-saarland.de>
2015-09-01 12:24:43 +0200

    test

--- tests/LED/LEDmatrix/LEDmatrix.ino
+++ tests/LED/LEDmatrix/LEDmatrix.ino
@@ -9,7 +9,7 @@
 #define PIN            6
 
 // How many NeoPixels are attached to the Arduino?
-#define NUMPIXELS      384
+#define NUMPIXELS      64
 
 // When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
 // Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
@@ -27,7 +27,7 @@ void setup() {
 
   pixels.begin(); // This initializes the NeoPixel library.
   delay(500);
-  int v = 16;
+  int v = 255;
   for(int i=0;i<NUMPIXELS;i++){
 
     // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
